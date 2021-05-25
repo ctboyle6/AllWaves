f9a0c26dda884d4dabeba76c5b5bbfe184522dea
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-25 12:28:18 -0400

    Added the spot model : name latitude:float longitude:float surfline_spot surfline_subregion

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_25_162553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spots", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "surfline_spot"
+    t.string "surfline_subregion"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
