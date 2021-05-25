835e7eb1aec02b046b7fef097a196edc5b7f0f7d
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-25 13:44:22 -0400

    Run migration, added has_many to the Spot and User models

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,3 @@
 class Spot < ApplicationRecord
+  has_many :user_spots
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_164000) do
+ActiveRecord::Schema.define(version: 2021_05_25_173805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2021_05_25_164000) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_spots", force: :cascade do |t|
+    t.bigint "spot_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_user_spots_on_spot_id"
+    t.index ["user_id"], name: "index_user_spots_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_05_25_164000) do
   end
 
   add_foreign_key "conditions", "spots"
+  add_foreign_key "user_spots", "spots"
+  add_foreign_key "user_spots", "users"
 end
