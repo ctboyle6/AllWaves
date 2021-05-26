b2080a70c5147480061d773957af5227dd4ed90f
Chris Boyle <cthboyle6@gmail.com>
2021-05-26 17:29:49 -0400

    added toggle to save button when form unlocked

--- app/javascript/components/init_formtoggle.js
+++ app/javascript/components/init_formtoggle.js
@@ -1,6 +1,7 @@
 const initFormtoggle = () => {
   const btn = document.getElementById('form-toggle');
   const form_items = document.querySelectorAll('.form-control')
+  const form_save = document.getElementById('form-save');
 
   if(btn) {
   btn.addEventListener('click', (event) => {
@@ -8,7 +9,9 @@ const initFormtoggle = () => {
 
     form_items.forEach((item) => {
       item.toggleAttribute("disabled");
-    })
+    });
+
+    form_save.style.display = form_save.style.display === 'none' ? '' : 'none';
   })
 }};
 
