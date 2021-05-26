91b47896c84bdea2c597957cea33743598733fe2
Clayton Landry <clayton.landry@gmail.com>
2021-05-26 14:19:58 -0400

    fixed /users to be /dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/user' => "dashboards#show", :as => :user_root
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :dashboard, only: [:show]
+  resource :dashboard, only: [:show], :as => :user_root
   resources :preferences
   resources :spots, only: [:index, :show, :create]
 end
