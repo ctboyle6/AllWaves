b850caad611d031e61f28e1b596435fe81b8345b
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-27 10:06:19 -0400

    Fixed bug and create a new function for creating spot

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../../db/spot_scrape'
+
 class SpotsController < ApplicationController
   def index
     if params[:query].present?
@@ -22,4 +24,31 @@ class SpotsController < ApplicationController
         lng: @spot.longitude
       }]
   end
+
+  def new
+    @spot = Spot.new
+  end
+  
+  def create
+    @spot = Spot.new(params[:name])
+    
+    
+    if @spot.save
+      flash[:success] = "Spot successfully created"
+      redirect_to @spot
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+    
+    url_spot_id = scrap_surfline_spot_id(location_human_to_query(params[:spot][:name]))
+    spot_id = get_id_location(url_spot_id)
+    
+    create_condition(@spot,spot_id)
+    
+    @spot.latitude = wind_json["associated"]["location"]["lat"]
+    @spot.longitude = wind_json["associated"]["location"]["lon"]
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :dashboard, only: [:show], :as => :user_root
   resources :preferences
-  resources :spots, only: [:index, :show, :create]
+  resources :spots, only: [:index, :show, :new, :create]
 end
