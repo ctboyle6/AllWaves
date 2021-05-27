4c6340953029ef66fe4ef350951e8515d6a70d4e
Chris Boyle <cthboyle6@gmail.com>
2021-05-27 08:35:22 -0400

    TODO: extract range.value to visibile element

--- app/javascript/components/init_formtoggle.js
+++ app/javascript/components/init_formtoggle.js
@@ -1,7 +1,19 @@
 const initFormtoggle = () => {
   const btn = document.getElementById('form-toggle');
   const form_items = document.querySelectorAll('.form-control')
+  const form_ranges = document.querySelectorAll('.numeric.range')
   const form_save = document.getElementById('form-save');
+
+  // console.log(form_ranges);
+  // form_items[3].insertAdjacentHTML('beforebegin', '<span>kooook</span>')
+
+  form_ranges.forEach((range) => {
+    const range_val = range.insertAdjacentHTML('beforebegin', `<span>${range.value}</span>`);
+    range.addEventListener('input', () => {
+      range.innerText = range.value;
+    });
+  });
+
 
   if(btn) {
   btn.addEventListener('click', (event) => {
