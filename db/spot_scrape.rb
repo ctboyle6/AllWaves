0f07b2b1e2903d0d274943d094fccbd139233bad
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-27 11:31:07 -0400

    Fixed the seeds

--- db/spot_scrape.rb
+++ db/spot_scrape.rb
@@ -62,6 +62,25 @@ def call_tide_api(spot_id_location)
   JSON.parse(tides_serialized)
 end
 
+def get_tides_variables(timestamp,results)
+  filtered_results =  results.select do |result| 
+    result["timestamp"] < ( timestamp + 3600*3 ) && result["timestamp"] >= timestamp
+  end
+  filtered_results_high = filtered_results.select {|result| result["type"] == "HIGH" }
+  filtered_results_low = filtered_results.select {|result| result["type"] == "LOW" }
+
+  if filtered_results_low.size > 0
+    @tide_type = "LOW"
+  elsif filtered_results_high.size > 0
+    @tide_type = "HIGH"
+  else 
+    @tide_type = "NORMAL"
+  end
+  filtered_results_high = 0
+  filtered_results_low = 0
+  pp 
+end
+
 
 
 # << conditions >>
@@ -89,49 +108,49 @@ def create_condition(new_spot,spot_id)
   results_tide = tide_json["data"]["tides"]
 
   # Keeping the same timestamps
-  results_wind.each do |result|
-    timestamp = result["timestamp"]
-    wind_strength = result["speed"]
-    wind_direction = result["direction"]
-    wind_gust = result["gust"]
-    wind_optimal_score = result["optimalScore"]
+ results_wind.each do |result|
+    @timestamp = result["timestamp"]
+    @wind_strength = result["speed"]
+    @wind_direction = result["direction"]
+    @wind_gust = result["gust"]
+    @wind_optimal_score = result["optimalScore"]
+    url_spot_id = scrap_surfline_spot_id(location_human_to_query("pipeline")) # TODO: interpolate search value
+    spot_id = get_id_location(url_spot_id)
+    tide_json = call_tide_api(spot_id)
+    results_tide = tide_json["data"]["tides"]
+    get_tides_variables(@timestamp, results_tide)
     results_wave.each do |result|
-      if timestamp == result["timestamp"]
-        waves_surf_min = result["surf"]["min"]
-        waves_surf_max = result["surf"]["max"]
-        waves_surf_optimal_score = result["surf"]["optimalScore"]
+      if result["timestamp"] == @timestamp
+        @waves_surf_min = result["surf"]["min"]
+        @waves_surf_max = result["surf"]["max"]
+        @waves_surf_optimal_score = result["surf"]["optimalScore"]
         # Logic to keep the highest swell
         biggest_swell = 0
         result["swells"].each do |swell|
           if swell["height"] > biggest_swell
-            biggest_swell = swell["height"]
+            @biggest_swell = swell["height"]
             @waves_swell_height = biggest_swell
             @waves_swell_period = swell["period"]
             @waves_swell_direction = swell["direction"]
             @waves_swell_direction_min = swell["directionMin"]
             @waves_swell_optimal_score = swell["optimalScore"]
-
           end
         end
-
         results_tide.each do |result|
-          if timestamp == result["timestamp"]
-            tide_type = result["type"]
-            tide_height = result["height"]
-
-            # Creating the new condition seed
-            condition = Condition.create!(
+          if @timestamp == result["timestamp"]
+            @tide_height = result["height"]
+            Condition.create!(
               spot_id: new_spot.id,
-              timestamp: timestamp,
-              wind_strength: wind_strength,
-              wind_direction: wind_direction,
-              wind_gust: wind_gust,
-              wind_optimal_score: wind_optimal_score,
-              waves_surf_min: waves_surf_min,
-              waves_surf_max: waves_surf_max,
-              waves_optimal_score: waves_surf_optimal_score,
-              tide_type: tide_type,
-              tide_height: tide_height,
+              timestamp: @timestamp,
+              wind_strength: @wind_strength,
+              wind_direction: @wind_direction,
+              wind_gust: @wind_gust,
+              wind_optimal_score: @wind_optimal_score,
+              waves_surf_min: @waves_surf_min,
+              waves_surf_max: @waves_surf_max,
+              waves_optimal_score: @waves_surf_optimal_score,
+              tide_type: @tide_type,
+              tide_height: @tide_height,
               waves_swell_height: @waves_swell_height,
               waves_swell_period: @waves_swell_period,
               waves_swell_direction: @waves_swell_direction,
