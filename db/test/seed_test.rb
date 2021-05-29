5e59f4293354deaca24bf3ebf69ba5c078d1170a
Chris Boyle <cthboyle6@gmail.com>
2021-05-29 13:10:38 -0400

    refactoring condition build in seed

--- db/test/seed_test.rb
+++ db/test/seed_test.rb
@@ -20,6 +20,7 @@ puts "Creating new spot names..."
 # wind_json = call_wind_api(spot_id)
 # new_spot.latitude = wind_json["associated"]["location"]["lat"]
 # new_spot.longitude = wind_json["associated"]["location"]["lon"]
+# ADD UTC OFFSET TO SPOT HERE
 
 # if new_spot.save
 #   puts "#{new_spot.name.capitalize} has been saved"
@@ -134,61 +135,90 @@ def create_condition(spot_id) #new_spot,
   results_tide = tide_json["data"]["tides"]
 
   # Keeping the same timestamps
- results_wind.each do |result|
-    @timestamp = result["timestamp"]
-    @wind_strength = result["speed"]
-    @wind_direction = result["direction"]
-    @wind_gust = result["gust"]
-    @wind_optimal_score = result["optimalScore"]
-    get_tides_variables(@timestamp, results_tide)
-    results_wave.each do |result|
-      if result["timestamp"] == @timestamp
-        @waves_surf_min = result["surf"]["min"]
-        @waves_surf_max = result["surf"]["max"]
-        @waves_surf_optimal_score = result["surf"]["optimalScore"]
-        # Logic to keep the highest swell
-        biggest_swell = 0
-        result["swells"].each do |swell|
-          if swell["height"] > biggest_swell
-            biggest_swell = swell["height"]
-            @waves_swell_height = biggest_swell
-            @waves_swell_period = swell["period"]
-            @waves_swell_direction = swell["direction"]
-            @waves_swell_direction_min = swell["directionMin"]
-            @waves_swell_optimal_score = swell["optimalScore"]
-          end
-        end
-        results_tide.each do |result|
-          if @timestamp == result["timestamp"]
-            p @tide_height = result["height"]
-            # Condition.create!(
-            #   spot_id: new_spot.id,
-            #   timestamp: @timestamp,
-            #   wind_strength: @wind_strength,
-            #   wind_direction: @wind_direction,
-            #   wind_gust: @wind_gust,
-            #   wind_optimal_score: @wind_optimal_score,
-            #   waves_surf_min: @waves_surf_min,
-            #   waves_surf_max: @waves_surf_max,
-            #   waves_optimal_score: @waves_surf_optimal_score,
-            #   tide_type: @tide_type,
-            #   tide_height: @tide_height,
-            #   waves_swell_height: @waves_swell_height,
-            #   waves_swell_period: @waves_swell_period,
-            #   waves_swell_direction: @waves_swell_direction,
-            #   waves_swell_direction_min: @waves_swell_direction_min,
-            #   waves_swell_optimal_score: @waves_swell_optimal_score
-            # )
-          end
-        end
-      end
-    end
+  timestamps = results_wind.map { |result| result["timestamp"] }
+
+  p timestamps
+
+  timestamps.each_with_index do |timestamp, index|
+    @timestamp = timestamp
+    @wind_strength = results_wind[index]["speed"]
+    @wind_direction = results_wind[index]["direction"]
+    @wind_direction = results_wind[index]["direction"]
   end
+
+  @condition = {
+    timestamp: @timestamp,
+    wind_strength: @wind_strength,
+    wind_direction: @wind_direction,
+    wind_gust: @wind_gust,
+    wind_optimal_score: @wind_optimal_score,
+    waves_surf_min: @waves_surf_min,
+    waves_surf_max: @waves_surf_max,
+    waves_optimal_score: @waves_surf_optimal_score,
+    tide_type: @tide_type,
+    tide_height: @tide_height,
+    waves_swell_height: @waves_swell_height,
+    waves_swell_period: @waves_swell_period,
+    waves_swell_direction: @waves_swell_direction,
+    waves_swell_direction_min: @waves_swell_direction_min,
+    waves_swell_optimal_score: @waves_swell_optimal_score
+  }
+
+  # results_wind.each do |result|
+  #   @timestamp = result["timestamp"]
+  #   @wind_strength = result["speed"]
+  #   @wind_direction = result["direction"]
+  #   @wind_gust = result["gust"]
+  #   @wind_optimal_score = result["optimalScore"]
+    # get_tides_variables(@timestamp, results_tide)
+  #   results_wave.each do |result|
+  #     if result["timestamp"] == @timestamp
+  #       @waves_surf_min = result["surf"]["min"]
+  #       @waves_surf_max = result["surf"]["max"]
+  #       @waves_surf_optimal_score = result["surf"]["optimalScore"]
+  #       # Logic to keep the highest swell
+  #       biggest_swell = 0
+  #       result["swells"].each do |swell|
+  #         if swell["height"] > biggest_swell
+  #           biggest_swell = swell["height"]
+  #           @waves_swell_height = biggest_swell
+  #           @waves_swell_period = swell["period"]
+  #           @waves_swell_direction = swell["direction"]
+  #           @waves_swell_direction_min = swell["directionMin"]
+  #           @waves_swell_optimal_score = swell["optimalScore"]
+  #         end
+  #       end
+  #       results_tide.each do |result|
+  #         if @timestamp == result["timestamp"]
+  #           @tide_height = result["height"]
+  #           # Condition.create!(
+  #           #   spot_id: new_spot.id,
+  #           #   timestamp: @timestamp,
+  #           #   wind_strength: @wind_strength,
+  #           #   wind_direction: @wind_direction,
+  #           #   wind_gust: @wind_gust,
+  #           #   wind_optimal_score: @wind_optimal_score,
+  #           #   waves_surf_min: @waves_surf_min,
+  #           #   waves_surf_max: @waves_surf_max,
+  #           #   waves_optimal_score: @waves_surf_optimal_score,
+  #           #   tide_type: @tide_type,
+  #           #   tide_height: @tide_height,
+  #           #   waves_swell_height: @waves_swell_height,
+  #           #   waves_swell_period: @waves_swell_period,
+  #           #   waves_swell_direction: @waves_swell_direction,
+  #           #   waves_swell_direction_min: @waves_swell_direction_min,
+  #           #   waves_swell_optimal_score: @waves_swell_optimal_score
+  #           # )
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
 
 # <<---- Conditions seeds ---->>
 puts "Getting conditions..."
 
-create_condition("5842041f4e65fad6a770888a") #new_spot,
+create_condition("5842041f4e65fad6a7708e1a") #new_spot,
 
 puts "Finished seeding conditions..."
