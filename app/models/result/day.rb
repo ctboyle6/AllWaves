7e849210dbb9f3581d54689350f286ff2a379f95
Clayton Landry <clayton.landry@gmail.com>
2021-05-29 18:58:05 -0400

    added spot.utc_offset to Day.conditions

--- app/models/result/day.rb
+++ app/models/result/day.rb
@@ -22,7 +22,11 @@ class Result
 
     def conditions
       # TODO: Add Spot.utc (seed file update) to @date to make tz dynamic
-      Condition.where(spot: @spot).where("timestamp BETWEEN ? AND ?", @date.midnight.to_i, @date.tomorrow.midnight.to_i)
+      Condition.where(spot: @spot).where("timestamp BETWEEN ? AND ?", @date.midnight.to_i - utc_seconds, @date.tomorrow.midnight.to_i - utc_seconds - 1)
+    end
+
+    def utc_seconds
+      @spot.utc_offset * 3600
     end
   end
 end
