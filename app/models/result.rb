d19cf829765f46cf817cbcfbb90b9373f19c7f3b
Clayton Landry <clayton.landry@gmail.com>
2021-05-29 13:11:19 -0400

    added result object

---
+++ app/models/result.rb
@@ -0,0 +1,16 @@
+class Result
+  def initialize(params)
+    @preference = params[:preference]
+    @spot       = params[:spot]
+
+    @start_date = params[:start_date] || Date.today
+    @days_count = params[:days_count] || 5
+    @end_date   = @start_date + @days_count
+  end
+
+  def days
+    (@start_date..@end_date).map do |date|
+      Day.new(preference: @preference, spot: @spot, date: date)
+    end
+  end
+end
