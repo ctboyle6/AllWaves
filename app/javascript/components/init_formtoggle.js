a0d36519b88aa8d6f546dd791319f64401540776
Chris Boyle <cthboyle6@gmail.com>
2021-05-30 11:41:46 -0400

    implemented single range value render using js

--- app/javascript/components/init_formtoggle.js
+++ app/javascript/components/init_formtoggle.js
@@ -4,15 +4,27 @@ const initFormtoggle = () => {
   const form_ranges = document.querySelectorAll('.numeric.range')
   const form_save = document.getElementById('form-save');
 
-  // console.log(form_ranges);
-  // form_items[3].insertAdjacentHTML('beforebegin', '<span>kooook</span>')
+  const render = (template, node) => {
+    node.innerHTML = template;
+  }
+
 
   // form_ranges.forEach((range) => {
   //   const range_val = range.insertAdjacentHTML('beforebegin', `<span>${range.value}</span>`);
   //   range.addEventListener('input', () => {
   //     range.innerText = range.value;
+  //     range_val.innerText = range.value;
   //   });
   // });
+
+  // console.log(document.querySelector('#test'));
+
+  const range_slide = document.getElementById('preference_swell_hgt_min');
+  range_slide.addEventListener('input', () => {
+    let template = `<span>${range_slide.value}</span>`;
+    render(template, document.querySelector('#test'));
+  });
+  // console.log(range_slide);
 
 
   if(btn) {
