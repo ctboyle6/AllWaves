4273d7372d5b91dbcbfdacb8c688a2a3606c06ff
Chris Boyle <cthboyle6@gmail.com>
2021-06-03 13:01:56 -0400

    new preference form into preference page, default preferences assigned to new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,17 @@ class User < ApplicationRecord
   has_many :preferences, dependent: :destroy
   has_one_attached :avatar
 
+  before_create :create_default_preferences
+
   def grouped_preferences
     preferences.order("favourite DESC")
+  end
+
+  private
+
+  def create_default_preferences
+    preferences.new(name: "Default - Rookie", pref_unit: "M", swell_hgt_min: 0.5, swell_hgt_max: 2, swell_int_min: 10, swell_int_max: 15, swell_dir_min: 150, swell_dir_max: 200, wind_str_min: 2, wind_str_max: 10, wind_dir_min: 200, wind_dir_max: 300, pref_tide_position: "LOW", pref_tide_range: 1 )
+    preferences.new(name: "Default - Mediocre", pref_unit: "M", swell_hgt_min: 0.5, swell_hgt_max: 2, swell_int_min: 10, swell_int_max: 15, swell_dir_min: 150, swell_dir_max: 200, wind_str_min: 2, wind_str_max: 10, wind_dir_min: 200, wind_dir_max: 300, pref_tide_position: "NORMAL", pref_tide_range: 2 )
+    preferences.new(name: "Default - Advanced", pref_unit: "M", swell_hgt_min: 0.5, swell_hgt_max: 2, swell_int_min: 10, swell_int_max: 15, swell_dir_min: 150, swell_dir_max: 200, wind_str_min: 2, wind_str_max: 10, wind_dir_min: 200, wind_dir_max: 300, pref_tide_position: "HIGH", pref_tide_range: 3 )
   end
 end
